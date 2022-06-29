6dc50b142cd0ece0950380cca169f1d83147f171
Light <47911535+LightAPIs@users.noreply.github.com>
2022-06-29 23:52:56 +0800

    fix: 修复歌名中包含"❤"符号时可能没有匹配结果的问题

--- src/followSource.php
+++ src/followSource.php
@@ -106,8 +106,8 @@ class AumFollowSource {
     private function getCleanStr($str) {
         $lowStr = strtolower($str);
         return str_replace(
-            array(" ", "，", "：", "；", "！", "？", "「", "」", "（", "）", "。"),
-            array("", ",", ":", ";", "!", "?", "｢", "｣", "(", ")", "."),
+            array(" ", "，", "：", "；", "！", "？", "「", "」", "（", "）", "。", "❤"),
+            array("", ",", ":", ";", "!", "?", "｢", "｣", "(", ")", ".", "♥"),
             $lowStr);
     }
 }
